b9e7739e626a236461e49ed6831e4d0966fc4eff
JCOMMERCE\patk <pawel.tkocz@jcommerce.pl>
2017-10-07 19:41:38 +0200

    Add connect and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
-const app = express();
 const port = process.env["PORT"] || 3000;
-
 const publicPath = path.join(__dirname, '/../public');
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log(`new user connected`);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected!!');
+  })
+});
+
+io.on('disconnected', () => {
+  console.log('io.disconnected');
+});
 
 
-console.log(publicPath);
 
-app.listen(port, () => {
+// start server
+server.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 })
